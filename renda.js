ed293a7f349450ae3455c60888d42fac660afdd8
Jin Aoyagi <contact.jin02@gmail.com>
2021-11-27 18:43:27 +0900

    時間制限つけた

--- renda.js
+++ renda.js
@@ -1,4 +1,21 @@
-// const pushBtn = document.getElementById('pushtBtn');
+const pushBtn = document.getElementById('pushBtn');
+
+const startTime = Date.now();
+let nowTime = 0;
+const showTime = document.getElementById('showTime');
+const LIMIT = 10;
+
+function time() {
+    nowTime = (Date.now() - startTime) / 1000;
+    let rest = LIMIT - Math.floor(nowTime);
+    if(rest > 0) {
+        showTime.innerText = `残り${rest}秒`;
+    } else {
+        showTime.innerText = '終了！！！';
+        pushBtn.style.pointerEvents = 'none';
+    }
+}
+
 const countBox = document.getElementById('countBox');
 let count = 0;
 
@@ -6,3 +23,5 @@ function plusOne() {
     count++
     countBox.innerText = count;
 }
+time();
+setInterval(time, 1000);
