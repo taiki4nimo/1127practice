897d75e1bb05af34204802104c90b5aa51025dae
Watanabe Eiki <basketball.slamdunk1@icloud.com>
2021-11-27 17:54:52 +0900

    みんなやってね

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const pop =document.getElementById('pop');
+const push=document.getElementById('push')
+
+
+function pushed(){
+pop.style.display="block"
+push.style.display="none"
+
+}
+
+push.addEventListener('click',pushed)
+
+
+
